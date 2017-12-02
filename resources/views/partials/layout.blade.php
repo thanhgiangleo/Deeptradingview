e85664654ece5aee1935312ba28d01ecea2f18a7
Dinh Trang Thanh Giang <thanhgiang.hcmus@gmail.com>
2017-12-02 16:50:28 +0700

    admin panel dashboard ver1

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -14,10 +14,18 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
     {{--<link rel="stylesheet" href="/css/styles.css" type="text/css">--}}
     <link rel="stylesheet" href="/css/payment.css" type="text/css">
+
+    <!-- Admin -->
+    <link href="/css/admin/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+    <link href="/css/admin/animate.min.css" rel="stylesheet"/>
+    <link href="/css/admin/bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/admin/pe-icon-7-stroke.css" rel="stylesheet" />
 </head>
 <body>
 
@@ -27,5 +35,17 @@
     <script src="/js/styles.js"></script>
     <script src="/js/loginBase.js"></script>
     <script src="/js/login.js"></script>
+
+    <!-- ADMIN -->
+    <script src="/js/admin/jquery.3.2.1.min.js" type="text/javascript"></script>
+    <script src="/js/admin/bootstrap.min.js" type="text/javascript"></script>
+    <!--  Charts Plugin -->
+    <script src="/js/admin/chartist.min.js"></script>
+    <!--  Notifications Plugin -->
+    <script src="/js/admin/bootstrap-notify.js"></script>
+    <!-- Light Bootstrap Table Core javascript -->
+    <script src="/js/admin/light-bootstrap-dashboard.js?v=1.4.0"></script>
+    <!-- Light Bootstrap Table DEMO methods -->
+    <script src="/js/admin/demo.js"></script>
 </footer>
 </html>
