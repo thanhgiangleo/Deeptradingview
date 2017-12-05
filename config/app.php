5ee04e4e3d8cb66e43a4d2ebce574b4d6f401613
Dinh Trang Thanh Giang <thanhgiang.hcmus@gmail.com>
2017-12-05 16:41:31 +0700

    add backpack for admin panel

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+
+        Backpack\Base\BaseServiceProvider::class,
     ],
 
     /*
